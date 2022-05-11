8e78ef05175996831dc1b116ac85dce4650c4295
mbulucay <ulucaybedir@gmail.com>
2022-05-11 23:27:37 +0300

    Scriptable Object

---
+++ quiz_master_project/Quiz Master/Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "aaa", menuName = "My OSOSOSO", order = 0)]
+
+public class NewBehaviourScript : ScriptableObject
+{
+
+    [SerializeField] string a;
+    public string b;
+    private string c;
+
+
+}
+
